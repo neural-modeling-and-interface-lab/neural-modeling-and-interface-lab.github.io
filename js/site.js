871864fab62561dcd89ac84fcb7e6cb6da5a8658
Garrett <gflynn@usc.edu>
2020-10-17 14:32:40 -0500

    Fixes for Dong

--- js/site.js
+++ js/site.js
@@ -1,9 +1,9 @@
 function menuToggle() {
     var x = document.getElementById("links");
     console.log('toggled')
-    if (x.style.top === "275px") {
-        x.style.top = "-225px";
+    if (x.style.top === "200px") {
+        x.style.top = "-205px";
     } else {
-        x.style.top = "275px";
+        x.style.top = "200px";
     }
 }
