d34e75b2e8300a7e37c4ac458cff3367cfb2910f
Dong Song <73083782+dongsong-lab@users.noreply.github.com>
2022-03-03 19:18:07 -0800

    Update current-people.js

--- components/current-people.js
+++ components/current-people.js
@@ -146,49 +146,6 @@ class CurrentPeople extends HTMLElement {
         <h1>Undergraduate Students</h1>
         <hr class="underheader"/>
 
-        <div id="garrett" class="feature">
-            <div class="feature-left">
-                <img src="assets/people/garrett_square.jpg"/>
-                <div class="social">
-                    <a href="https://www.linkedin.com/in/garrettmflynn/">
-                        <div>
-                            <svg viewBox="0 0 20 24" width="20" ><use xlink:href="#linkedin"/></svg>
-                        </div>
-                    </a>
-                    <a href="https://github.com/garrettmflynn">
-                        <div>
-                            <svg viewBox="0 0 20 20" width="20" ><use xlink:href="#github"/></svg>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="feature-text margin-left">
-                <h2 class="top-margin">Garrett Flynn</h2>
-                <p>Garrett Flynn is a progressive degree student in the Computational Neuroscience (BS) and Media Arts, Games, and Health (MA) programs. Working at the intersection of neuroscience, ethics, and interactive media, he leads several interdisciplinary projects out of the Neural Modeling and Interface Lab.
-                </p><p>When not dreaming up new ways to incorporate end-user feedback into neurotechnology design, you can find Garrett on long bike rides in his home state of Wisconsin.
-                </p>
-            </div>
-        </div>
-
-        <div id="natalie" class="feature">
-            <div class="feature-left">
-                <img src="assets/people/natalie_square.jpg"/>
-                <div class="social">
-                    <a href="https://www.linkedin.com/in/natalie-kistler-9b9545194/">
-                        <div>
-                            <svg viewBox="0 0 20 20" width="20" ><use xlink:href="#linkedin"/></svg>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="feature-text margin-left">
-                <h2 class="top-margin">Natalie Kistler</h2>
-                <p>Natalie Kistler is a Biomedical Engineering student and on the Pre-Med track in hopes of pursuing medical school after completing her undergraduate degree. Natalie is currently working on a cooperative project between the Neural Modeling and Interface Lab and Keck Medical School regarding Transcranial Magnetic Stimulation (TMS) and its implementation in the treatment of epilepsy.
-                </p><p>In addition to her research, Natalie is a self-proclaimed “foodie” and is on the hunt for the best restaurant in Los Angeles.
-                </p>
-            </div>
-        </div>
-
         <div id="yilei" class="feature">
             <div class="feature-left">
                 <img src="assets/people/yilei_square.jpg"/>
@@ -224,23 +181,6 @@ class CurrentPeople extends HTMLElement {
             </div>
         </div>
 
-        <div id="robert" class="feature">
-            <div class="feature-left">
-                <img src="assets/people/robert_square.jpg"/>
-                <div class="social">
-                    <a href="https://www.linkedin.com/in/robert-sutherland-4a065b173/">
-                        <div>
-                            <svg viewBox="0 0 20 24" width="20" ><use xlink:href="#linkedin"/></svg>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="feature-text margin-left">
-                <h2 class="top-margin">Robert Sutherland</h2>
-                <p>Robert Sutherland joined the lab in the summer of 2021.  He has had a wide range of experience in the medical device space including working in multiple labs, researching BCI at the Charite in Berlin, presenting at the NSF emerging researcher conference, working as an intern at the FDA, and completing an R&D engineering internship at Boston Scientific.</p>
-				<p>He is currently working on his master’s degree in Neuro-Engineering, and his research is focusing on constructing signals processing pipelines that boost performance of machine learning classifiers.  In his free time, Robert likes to draw, ski, dive and travel. </p>
-            </div>
-        </div>
     `;
     }
 }
